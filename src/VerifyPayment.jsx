1b8e7ac72c8f44fcb355bf32a8fef29bf0e11a46
Hithashree A M <hithashree1384@gmail.com>
2026-05-01 18:00:33 +0530

    new error  fix

--- src/VerifyPayment.jsx
+++ src/VerifyPayment.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
-import { API_BASE } from "../config";
+import { API_BASE } from "./config";
 
 
 const VerifyPayment = () => {
